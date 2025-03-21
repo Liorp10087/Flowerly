a3578aa5d2eac5215f53a673a76fe1ce87f6929a
Tal Finkelshtein <finkelshtein.tal@gmail.com>
2025-03-21 13:50:09 +0200

    add real user to post

--- app/src/main/java/com/example/flowerly/dao/AppLocalDatabase.kt
+++ app/src/main/java/com/example/flowerly/dao/AppLocalDatabase.kt
@@ -22,7 +22,7 @@ abstract class AppDatabase : RoomDatabase() {
                     context.applicationContext,
                     AppDatabase::class.java,
                     "app_database"
-                ).fallbackToDestructiveMigration() 
+                ).fallbackToDestructiveMigration()
                     .build()
                 INSTANCE = instance
                 instance
